bae249dc57bf3f08f191f256c6484ff49dadb9b2
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-07-31 16:59:02 -0300

    Correçao nas configuraçoes de deploy

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -10,6 +10,6 @@ const connection = {
   connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+if (process.env.NODE_ENV === "production") connection.ssl = true;
 
 export const db = new Pool(connection)
