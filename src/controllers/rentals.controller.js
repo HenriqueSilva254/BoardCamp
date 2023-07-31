a49b24db8f817190979eade0385b715ba14c8dc1
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-07-31 15:16:02 -0300

    feat: function dellRental

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -57,7 +57,7 @@ export async function finishRental(req, res){
     try { 
         const checkGameId = await db.query(`SELECT rentals."gameId" FROM rentals WHERE id=$1`, [id])
         if(checkGameId.rows.length === 0) return res.status(404).send('Aluguel não existe') 
-        const checkRentalFinish = await db.query(`SELECT  rentals."returnDate", rentals."rentDate" FROM rentals WHERE id=$1`, [id])
+        const checkRentalFinish = await db.query(`SELECT  rentals."returnDate" FROM rentals WHERE id=$1`, [id])
         if(checkRentalFinish.rows.length > 0 && checkRentalFinish.rows[0].returnDate !== null) return res.status(400).send("Este aluguel já foi finalizado")
         
 
@@ -79,3 +79,20 @@ export async function finishRental(req, res){
         res.status(500).send(err.message)
     }
 }
+
+export async function dellRental(req, res){
+    const {id} = req.params
+
+    try {
+        const checkRentalFinish = await db.query(`SELECT  rentals."returnDate" FROM rentals WHERE id=$1`, [id])
+        if(checkRentalFinish.rows.length === 0) return res.status(404).send("Este aluguel não existe")
+        if(checkRentalFinish.rows[0].returnDate === null) return res.status(400).send("Este aluguel não foi finalizado")
+        
+        const Customers =  await db.query(`DELETE FROM rentals
+        WHERE id = $1`, [id])
+        res.status(200).send('Aluguel deletado')
+        
+    } catch (err) {
+          res.status(500).send(err.message)
+    }
+} 
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { finishRental, getRental, postRental } from "../controllers/rentals.controller.js";
+import { dellRental, finishRental, getRental, postRental } from "../controllers/rentals.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import rentalSchema from "../schemas/rentals.schema.js";
 
@@ -8,5 +8,6 @@ const rentals = Router()
 rentals.get("/rentals", getRental)
 rentals.post("/rentals", validateSchema(rentalSchema), postRental)
 rentals.post("/rentals/:id/return", finishRental)
+rentals.delete("/rentals/:id", dellRental)
 
 export default rentals 
